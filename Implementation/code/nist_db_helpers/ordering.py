1883f278e7558235135378af63f696705a114232
LE TRUNG HIEU <lth08091998@gmail.com>
2020-03-22 15:05:38 +0800

    Fix bug in svd ordering of mismatch matrix

--- Implementation/code/nist_db_helpers/ordering.py
+++ Implementation/code/nist_db_helpers/ordering.py
@@ -1,6 +1,10 @@
 # This class is responsible for atoms ordering functions to remove symmetry problem.
 import numpy as np
 
+def argsort(seq):
+    # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
+    return sorted(range(len(seq)), key=seq.__getitem__)
+
 # E is always (13, 13), so we want to extract sub-matrix of it before calculating the orders.
 class OrderingBase:
     @classmethod
@@ -27,14 +31,15 @@ class SvdOrdering(OrderingBase):
         # return list of idxes order and updated_adj_matrix
         sub_E = cls._extract_sub_adj_matrix(E, len(vertex_arr))
         u, s, v = np.linalg.svd(sub_E)
-        atoms_repr = np.dot(u, s)
+        atoms_repr = np.dot(u, np.diag(s))
         # iterate through each atom types
         current_adj_row_idx = 0
         for atom_type in range(0, max(vertex_arr)+1):
             row_idxes = np.where(np.array(vertex_arr) == atom_type)[0]
 
             #get orders of the atoms_reprs vectors
-            sort_idxes = np.argsort(atoms_repr[row_idxes])
+            # TODO: Experiment different sorting criterias???
+            sort_idxes = argsort(atoms_repr[row_idxes].tolist())
 
             # doing selection sort
             for i in range(len(row_idxes)):
